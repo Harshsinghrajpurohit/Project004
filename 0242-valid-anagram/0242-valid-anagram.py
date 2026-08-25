be4e3fc7bed33eda98a3b316e83618808d96b93e
Harsh Singh Rajpurohit <harshrajpurohit412005@gmail.com>
2026-08-25 12:48:25 +0530

    Time: 11 ms (77.27%), Space: 19.5 MB (46.35%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        freq={}
+        for ch in s:
+            if ch  in freq:
+                freq[ch]+=1
+            else:
+                freq[ch]=1
+        for ch in t:
+            if ch not in freq:
+                return False
+            freq[ch]-=1
+        for ch in freq:
+            if freq[ch]!=0:
+                return False
+            else:
+                return True
+                
